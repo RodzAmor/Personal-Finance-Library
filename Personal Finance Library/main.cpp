1e6161c35b2660d9887e19cafefba1da53601dae
Rodz Andrie Amor <ramorstem2022@example.com>
2023-10-04 16:48:14 -0400

    Initial Commit

---
+++ Personal Finance Library/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Personal Finance Library
+//
+//  Created by Andrie on 10/4/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
